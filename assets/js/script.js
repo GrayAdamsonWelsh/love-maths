b9d006e4d71adb52a6aa03b5951766d11b0a1de3
Graeme Adamson <graya0277@gmail.com>
2024-01-25 15:04:47 +0000

    scructure

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+    
+}
+
+function calculateCorrectAnswer(){
+    
+}
+
+function incrumentScore(){
+    
+}
+
+function incrumentWrongAnswer(){
+    
+}
+
+function dislayAdditionQuestion(){
+    
+}
+
+function displaySubtractionQuestion(){
+    
+}
+
+function dislayAdditionMultiplyQuestion
+(){
+    
+}
