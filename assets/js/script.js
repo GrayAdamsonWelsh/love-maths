3efd3ac40bc5be9d3c46e8d79d398b4bf31a52c3
Graeme Adamson <graya0277@gmail.com>
2024-01-26 16:11:05 +0000

    addition game added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,90 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByTagName("button")
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-    for (button of buttons) {
-        button.addEventListener("click", function(){
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(gameType);
-            } 
-        })
+                runGame(gameType);
+            }
+        });
     }
-})
 
+    runGame("addition");
 
-function runGame(){
+});
+
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
+
+    // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
-function checkAnswer(){
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
+}
+
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    
+    if (isCorrect) {
+        alert("Correct");
+    } else {
+        alert("Wrong, answer is " + calculatedAnswer);
+    }
+    runGame(calculatedAnswer);
+}
+
+function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById("operand1").innerHTML);
+    let operand2 = parseInt(document.getElementById("operand2").innerHTML);
+    let operator = document.getElementById("operator").innerHTML;
+    
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"]
+    } else {
+        alert("Unknown operator");
+    }
+
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
     
 }
 
-function calculateCorrectAnswer(){
-    
+function displaySubtractQuestion() {
+
 }
 
-function incrumentScore(){
-    
-}
-
-function incrumentWrongAnswer(){
-    
-}
-
-function dislayAdditionQuestion(){
-    
-}
-
-function displaySubtractionQuestion(){
-    
-}
-
-function dislayAdditionMultiplyQuestion
-(){
+function displayMultiplyQuestion() {
     
 }
